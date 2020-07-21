bd9b1aec6a011bb4b652f1c6a0bc06f4717328f3
Rytis Reventas <rytis.reventas@netcode.lt>
2020-07-21 23:24:35 +0300

    callRecordDetail import timestamp fix

--- src/main/java/com/craftsoft/callDetailRecord/controllers/impl/ImportControllerImpl.java
+++ src/main/java/com/craftsoft/callDetailRecord/controllers/impl/ImportControllerImpl.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ImportControllerImpl implements ImportController {
@@ -24,6 +27,21 @@ public class ImportControllerImpl implements ImportController {
     public List<CallRecordDetails> importCallRecordDetailsList(MultipartFile file, String fileUrl) throws Exception {
         var delimiter = ',';
         var callRecordDetailsList = importService.loadObjectList(CallRecordDetails.class,file, fileUrl, delimiter);
+        callRecordDetailsList.forEach(x-> {
+            var startDateSecondTimestamp = x.getStartDate();
+            var startDateMilliTimestamp = getMilliTimeStamp(startDateSecondTimestamp);
+            x.setStartDate(startDateMilliTimestamp);
+
+            var endDateSecondTimestamp = x.getEndDate();
+            var endDateMilliTimestamp = getMilliTimeStamp(endDateSecondTimestamp);
+            x.setEndDate(endDateMilliTimestamp);
+        });
         return callRecordService.save(callRecordDetailsList);
+    }
+
+    private Timestamp getMilliTimeStamp(Timestamp secondTimeStamp) {
+        var secondCount = secondTimeStamp.getTime();
+        var milliCount = TimeUnit.SECONDS.toMillis(secondCount);
+        return new Timestamp(milliCount);
     }
 }
